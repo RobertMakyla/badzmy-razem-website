5205331618ae0e3ece32bb72f34770f663c5da4f
robert.makyla <robert.makyla@gmail.com>
2017-04-22 10:35:38 +0200

    changing dir name + formatting whitespaces

--- generate.sh
+++ generate.sh
@@ -4,102 +4,102 @@ target=./temporary-target-dir
 
 array=( "index.html:O nas"
         "aktualnosci.html:Aktualności"
-  		"turnusy.html:Turnusy"
-    	"galeria.html:Galeria"
-		"prawne.html:Regulamin / Status"
+        "turnusy.html:Turnusy"
+        "galeria.html:Galeria"
+        "prawne.html:Regulamin / Status"
         "kontakt.html:Kontakt" )
 
 creatingEmptyHTML () {
-	for mapping in "${array[@]}" ; do
-		fName="${mapping%%:*}"
-		echo "Creating empty ${target}/${fName}"	
-		touch ${target}/${fName} 
-	done
+   for mapping in "${array[@]}" ; do
+      fName="${mapping%%:*}"
+      echo "Creating empty ${target}/${fName}"   
+      touch ${target}/${fName} 
+   done
 } 
 
 updatingWithCommon () {
-	commonsDir=commons
-	sourceFile=${commonsDir}/$1
-	for mapping in "${array[@]}" ; do
-		fName="${mapping%%:*}"
-		echo "Updating ${target}/${fName} with ${sourceFile}"	
-	
-		if [ -f ${sourceFile} ] ; then
-			cat ${sourceFile} >> ${target}/${fName} 
-		else 
-			echo "File ${sourceFile} does not exist"
-			exit 1
-		fi
-	done
+   commonsDir=common-parts
+   sourceFile=${commonsDir}/$1
+   for mapping in "${array[@]}" ; do
+      fName="${mapping%%:*}"
+      echo "Updating ${target}/${fName} with ${sourceFile}"   
+   
+      if [ -f ${sourceFile} ] ; then
+         cat ${sourceFile} >> ${target}/${fName} 
+      else 
+         echo "File ${sourceFile} does not exist"
+         exit 1
+      fi
+   done
 } 
 
 addMenuBar () {
-	for mapping in "${array[@]}" ; do
-		fName="${mapping%%:*}"
-	
-		for menubarMapping in "${array[@]}" ; do
-			menubarFName="${menubarMapping%%:*}"
-		    menubarTitle="${menubarMapping##*:}"
+   for mapping in "${array[@]}" ; do
+      fName="${mapping%%:*}"
+   
+      for menubarMapping in "${array[@]}" ; do
+         menubarFName="${menubarMapping%%:*}"
+          menubarTitle="${menubarMapping##*:}"
 
-			if [[ "${fName}" == "${menubarFName}" ]] ; then
-	   		    echo "Adding menu-bar for ${fName} with title ${menubarTitle}"	
-				echo "          <li class='selected'><a href='${menubarFName}'>${menubarTitle}</a></li>" >> ${target}/${fName} 
-			else 
-				echo "          <li><a href='${menubarFName}'>${menubarTitle}</a></li>" >> ${target}/${fName} 
-			fi
-		done
-		
-	done
+         if [[ "${fName}" == "${menubarFName}" ]] ; then
+                echo "Adding menu-bar for ${fName} with title ${menubarTitle}"   
+            echo "          <li class='selected'><a href='${menubarFName}'>${menubarTitle}</a></li>" >> ${target}/${fName} 
+         else 
+            echo "          <li><a href='${menubarFName}'>${menubarTitle}</a></li>" >> ${target}/${fName} 
+         fi
+      done
+      
+   done
 } 
 
 addPageContent () {
-	pagesDir=pages
+   pagesDir=pages
 
-	for mapping in "${array[@]}" ; do
-		fName="${mapping%%:*}"
-		title="${mapping##*:}"
-		
-		if [ -f ${pagesDir}/${fName} ] ; then
-			echo "Adding page content from ${pagesDir}/${fName} to ${target}/${fName}"	
-			cat ${pagesDir}/${fName} >> ${target}/${fName} 
-		else 
-			echo "Adding empty content to ${target}/${fName}"	
-			echo "        <h1>${title}</h1>"      >> ${target}/${fName} 
-			echo "        <p>Dział w budowie</p>" >> ${target}/${fName} 
-		fi
-		
-	done
+   for mapping in "${array[@]}" ; do
+      fName="${mapping%%:*}"
+      title="${mapping##*:}"
+      
+      if [ -f ${pagesDir}/${fName} ] ; then
+         echo "Adding page content from ${pagesDir}/${fName} to ${target}/${fName}"   
+         cat ${pagesDir}/${fName} >> ${target}/${fName} 
+      else 
+         echo "Adding empty content to ${target}/${fName}"   
+         echo "        <h1>${title}</h1>"      >> ${target}/${fName} 
+         echo "        <p>Dział w budowie</p>" >> ${target}/${fName} 
+      fi
+      
+   done
 }
 
 copyStyleAndImages () {
-	cp -fr style ${target}
+   cp -fr style ${target}
 }
 
 checkHTMLPages () {
-	for mapping in "${array[@]}" ; do
-		fName="${mapping%%:*}"
+   for mapping in "${array[@]}" ; do
+      fName="${mapping%%:*}"
 
-		echo "Checking HTML syntax of ${target}/${fName}"	
-		
-		tidy -utf8 -xml -q -e ${target}/${fName}
-	done
+      echo "Checking HTML syntax of ${target}/${fName}"   
+      
+      tidy -utf8 -xml -q -e ${target}/${fName}
+   done
 }
 
 main () {
-	echo "Creating fresh target dir ${target}"
-	rm -rf ${target}
-	mkdir -p ${target}
+   echo "Creating fresh target dir ${target}"
+   rm -rf ${target}
+   mkdir -p ${target}
 
-	creatingEmptyHTML
-	updatingWithCommon header.txt
-	addMenuBar
-	updatingWithCommon news.txt
-	addPageContent
-	updatingWithCommon footer.txt
-	
-	copyStyleAndImages
-	
-	checkHTMLPages
+   creatingEmptyHTML
+   updatingWithCommon header.txt
+   addMenuBar
+   updatingWithCommon news.txt
+   addPageContent
+   updatingWithCommon footer.txt
+   
+   copyStyleAndImages
+   
+   checkHTMLPages
 }
 
 main
